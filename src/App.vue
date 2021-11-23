8ccb5038fcd2df5e9f008450108998f14cd355a1
unknown <mdegroot09@gmail.com>
2021-11-23 16:52:45 -0700

    Added HighScores component and tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <HighScores/>
+</template>
+
+<script>
+import HighScores from './components/HighScores.vue'
+import movieQuote from 'popular-movie-quotes'
+import './assets/tailwind.css'
+
+export default {
+  name: 'App',
+  components: {
+    HighScores
+  },
+  mounted(){
+    let quotes = movieQuote.getAll()
+    console.log(quotes)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: darkslateblue;
+}
+</style>
---
+++ src/components/HighScores.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="scores w-screen h-auto font-bold">
+        <h3>Level: Novice</h3>
+        <h3>High Score: 0</h3>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.scores {
+    margin: 5px 0 0 10px;
+}
+</style>
