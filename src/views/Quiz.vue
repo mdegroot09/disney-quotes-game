8ca709c9a253ae8151146d18a892fc3f07021a31
unknown <mdegroot09@gmail.com>
2021-11-29 16:17:01 -0700

    Added layout to Quiz.vue

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,7 +1,22 @@
 <template>
     <div class="quiz w-full h-full grid justify-center items-center">
-        <div class="quizModule">
-
+        <div class="quizModule grid grid-rows-2">
+            <div class="grid justify-center items-center text-3xl">Quote 1/10</div>
+            <div class="question grid grid-rows-5 justify-items-start text-4xl">
+                <p class="grid w-full justify-center items-center px-6 py-8 text-6xl">"I am a really difficult quote"</p>
+                <p class="grid items-center px-6">
+                    <button>movie 1</button>
+                </p>
+                <p class="grid items-center px-6">
+                    <button>movie 2</button>
+                </p>
+                <p class="grid items-center px-6">
+                    <button>movie 3</button>
+                </p>
+                <p class="grid items-center px-6">
+                    <button>movie 4</button>
+                </p>
+            </div>
         </div>
     </div>
 </template>
@@ -22,6 +37,9 @@ export default {
     max-width: 800px;
     height: calc(100vh - 200px);
     max-height: 600px;
-    background: blue;
+    grid-template-rows: auto 1fr;
+}
+.question {
+    grid-template-rows: auto repeat(5, 1fr);
 }
 </style>
