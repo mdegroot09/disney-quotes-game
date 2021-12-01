dd4b1012029415c60ef11245470e56b54986a94e
unknown <mdegroot09@gmail.com>
2021-12-01 14:38:47 -0700

    Response functionality, next button show/hide

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="quiz w-full h-full grid justify-center items-center">
-        <div class="quizModule grid grid-rows-2">
+        <div class="quizModule grid grid-rows-3">
             <div class="grid grid-cols-2 text-3xl">
-                <span class="grid justify-start items-center">Quote #1</span>
-                <span class="grid justify-end items-center">Score: {{currentScore}}/{{currentQuestion}}</span>
+                <span class="grid justify-start items-center">Quote #{{currentQuestion + 1}}</span>
+                <span class="grid justify-end items-center">Score: {{currentScore}}/{{completedQuestions}}</span>
             </div>
             <div class="question grid grid-rows-5 justify-items-start text-4xl">
                 <p class="w-full text-center px-6 py-8 text-6xl">"{{correctQuote}}"</p>
                 <p class="grid items-center px-6">
-                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(0)">a. {{movie1}}</button>
+                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(0)" ref="movie0">a. {{movie0}}</button>
                 </p>
                 <p class="grid items-center px-6">
-                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(1)">b. {{movie2}}</button>
+                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(1)" ref="movie1">b. {{movie1}}</button>
                 </p>
                 <p class="grid items-center px-6">
-                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(2)">c. {{movie3}}</button>
+                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(2)" ref="movie2">c. {{movie2}}</button>
                 </p>
                 <p class="grid items-center px-6">
-                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(3)">d. {{movie4}}</button>
+                    <button class="choice hover:text-blue-800" @click="this.checkIfCorrect(3)" ref="movie3">d. {{movie3}}</button>
                 </p>
+            </div>
+            <div class="grid justify-center">
+                <button class="btn hide" @click="this.nextQuestion()" ref="nextBtn">next</button>
             </div>
         </div>
     </div>
@@ -36,46 +39,74 @@ export default {
     methods: {
         initiateQuiz(){
             let quotes = movieQuote.getSomeRandom(40)
-            console.log(quotes)
-
-            let movie1 = quotes[0].movie
-            let movie2 = quotes[1].movie
-            let movie3 = quotes[2].movie
-            let movie4 = quotes[3].movie
+            let movie0 = quotes[0].movie
+            let movie1 = quotes[1].movie
+            let movie2 = quotes[2].movie
+            let movie3 = quotes[3].movie
             let correctIndex = Math.floor(Math.random() * 4)
             let correctQuote = quotes[correctIndex].quote
+            let completedQuestions = 0
             let currentQuestion = 0
+            let responses = [{correctIndex, choice: null}]
 
             return {
                 quotes,
+                movie0,
                 movie1,
                 movie2,
                 movie3,
-                movie4,
                 correctIndex,
                 correctQuote,
-                currentQuestion
+                completedQuestions,
+                currentQuestion,
+                responses
             }
         },
         checkIfCorrect(i){
-            if(this.currentQuestion >= 10){return}
+            if(this.completedQuestions >= 10){
+                this.$refs['nextBtn'].classList.value = 'hide'
+                return
+            }
+            this.completedQuestions++
+            if(this.completedQuestions == 10){
+                this.$refs['nextBtn'].classList.value = 'hide'
+            }
+            else {
+                this.$refs['nextBtn'].classList.value = 'btn'
+            }
             if(i === this.correctIndex){
                 this.$store.commit('updateCurrentScore')
+                let correctRef = `movie${i}`
+                this.$refs[correctRef].classList.value = 'choice text-green-600'
             }
-            this.nextQuestion()
+            else {
+                let incorrectRef = `movie${i}` 
+                this.$refs[incorrectRef].classList.value = 'choice text-red-600'
+
+                let correctRef = `movie${this.correctIndex}`
+                this.$refs[correctRef].classList.value = 'choice text-green-600'
+            }
+        },
+        resetDisplays(){
+            this.$refs['movie0'].classList.value = 'choice hover:text-blue-800'
+            this.$refs['movie1'].classList.value = 'choice hover:text-blue-800'
+            this.$refs['movie2'].classList.value = 'choice hover:text-blue-800'
+            this.$refs['movie3'].classList.value = 'choice hover:text-blue-800'
         },
         nextQuestion(){
-            console.log(this.correctIndex)
-            console.log(this.quotes[(this.currentQuestion * 4) + this.correctIndex].movie)
-            console.log(this.correctQuote)
+            this.$refs['nextBtn'].classList.value = 'btn hide'
             this.currentQuestion++
-            if (this.currentQuestion < 10){
-                this.movie1 = this.quotes[this.currentQuestion * 4].movie
-                this.movie2 = this.quotes[this.currentQuestion * 4 + 1].movie
-                this.movie3 = this.quotes[this.currentQuestion * 4 + 2].movie
-                this.movie4 = this.quotes[this.currentQuestion * 4 + 3].movie
+            if(this.completedQuestions >= 10){
+                return
+            }
+            if (this.completedQuestions < 10){
+                this.resetDisplays()
+                this.movie0 = this.quotes[this.completedQuestions * 4].movie
+                this.movie1 = this.quotes[this.completedQuestions * 4 + 1].movie
+                this.movie2 = this.quotes[this.completedQuestions * 4 + 2].movie
+                this.movie3 = this.quotes[this.completedQuestions * 4 + 3].movie
                 this.correctIndex = Math.floor(Math.random() * 4)
-                this.correctQuote = this.quotes[(this.currentQuestion * 4) + this.correctIndex].quote
+                this.correctQuote = this.quotes[(this.completedQuestions * 4) + this.correctIndex].quote
             }
         }
     },
@@ -99,6 +130,23 @@ export default {
     grid-template-rows: auto 1fr;
 }
 .question {
-    grid-template-rows: auto repeat(5, 1fr);
+    grid-template-rows: auto repeat(4, 1fr);
+}
+.btn {
+    font-size: 1.5rem/* 24px */;
+    line-height: 2rem/* 32px */;
+    --tw-bg-opacity: 1;
+    background-color: rgba(30, 64, 175, var(--tw-bg-opacity));
+    border-width: 2px; 
+    width: 7rem/* 112px */;
+    height: 3rem/* 48px */;
+    margin-bottom: 2.5rem/* 40px */;
+    border-radius: 0.5rem/* 8px */;
+    --tw-text-opacity: 1;
+    color: rgba(255, 255, 255, var(--tw-text-opacity));
+    font-weight: 600;
+}
+.hide {
+    opacity: 0;
 }
 </style>
